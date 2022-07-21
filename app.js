4022a73188a432146e28e3902517e1e9560e2214
M.Ebad Arshad <104248177+ebad-arshad@users.noreply.github.com>
2022-07-21 21:06:09 +0500

    completed head tails & dice & password generator

---
+++ app.js
@@ -0,0 +1,35 @@
+// Head and Tails Game
+
+// var randomNumber = Math.ceil(Math.random() * 2);
+// document.write("Head and Tails Game")
+// if (randomNumber < 2) {
+//     alert("Heads")
+// }
+// else {
+//     alert ("Tails")
+// }
+
+// Dice Game
+
+// var randomNumber = Math.ceil(Math.random() * 6);
+// document.write("Dice Game");
+// document.write("<br>");
+// document.write("Go " + randomNumber + " moves forward");
+
+// Password Generator
+
+// var user = +prompt("Enter password length")
+// var randomStrings = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+"
+
+// var randomPassword = "";
+// var randomNumber = 0;
+// for (var i = 0; i < user; i++) {
+//     randomNumber = Math.floor(Math.random() * randomStrings.length);
+//     randomPassword += randomStrings.charAt(randomNumber);
+// }
+// console.log(randomPassword);
+
+
+
+
+
